9ce84c2dccbd0d0de9d9a72cf14bab0f53aaaec3
Jeongin Choi <jechoi@c3r3s3.42seoul.kr>
2021-03-06 22:31:17 +0900

    42

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jechoi </var/mail/jechoi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 22:05:43 by jechoi            #+#    #+#             */
-/*   Updated: 2021/03/06 22:08:33 by jechoi           ###   ########.fr       */
+/*   Updated: 2021/03/06 22:31:12 by jechoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,28 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	start;
+	size_t	end;
+	char	*ret;
 
+	if (!s1)
+		return (0);
+	if (!set)
+		return (ft_strdup(s1));
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (s1[end - 1] && ft_strchr(set, s1[end - 1]))
+	{
+		if (end - 1 < 1)
+			break ;
+		end--;
+	}
+	if (start > end)
+		return (ft_strdup(""));
+	if (!(ret = (char *)malloc(sizeof(char) * (end - start + 1))))
+		return (0);
+	ft_strlcpy(ret, s1 + start, end - start + 1);
+	return (ret);
+}
